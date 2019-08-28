6861f31157055d4616fd940afe95e1ba22daff82
Murilo Henrique <34652210+murilothink@users.noreply.github.com>
2019-08-27 21:27:44 -0300

    Update applying a scikit-learn.py

--- applying a scikit-learn.py
+++ applying a scikit-learn.py
@@ -7,37 +7,48 @@
 from sklearn import tree
 
 pera = 1
-verde = 1
 morango = 2
-vermelho =2
 banana = 3
-amarela = 3
 Laranja = 4
-Laranja1 = 4
+maca = 2
+melancia = 2
+abacaxi = 1
 
-pomar = [[4, 1],[7,2],[6,3],[7,4]]
+pomar = [[4,4,1],[7,4,2],[6,1,3],[7,3,4],[4,4,2], [8,4,1], [7,1,1]]
 
-resultado = [1,2,3,4]
+resultado = [1,2,3,4,5,6,7]
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(pomar, resultado)
 
-input('Escolha uma fruta e memorize: \n BANANA \n PERA \n MORANGO \n LARANJA \n Digite "Ok" para continuar  \n')
+input('Escolha uma dessas frutas e memorize: BANANA  PERA  MORANGO  LARANJA  MAÇA  MELANCIA  ABACAXI \n Digite "Ok" para continuar  ')
 
-letras = input("Quantas letras sua fruta tem?")
-cor = input("ENTRE COM A COR \n 1 Verde \n 2 Vermelha\n 3 Amarela \n 4 Laranja")
+peso = input(" \n Quantas letras sua fruta tem?")
 
-resultadoUsuario = clf.predict([[letras, cor]])
+primeira_letra = input(" \n ESCOLHA UM GRUPO QUE CONSIDERE A PRIMEIRA LETRA DA SUA FRUTA \n ABCD Digite 1 \n EFGH Digite 2 \n IJKL Digite 3 \n MNOP Digite 4 \n")
+
+surpefice = input(" \n ENTRE COM A COR \n 1 Verde \n 2 Vermelha\n 3 Amarela \n 4 Laranja")
+
+resultadoUsuario = clf.predict([[peso,primeira_letra, surpefice]])
 
 if  resultadoUsuario == 1:
     print(" \n Sua fruta é Pera")
 elif resultadoUsuario == 2:
-    print(" \n Sua fruta é Morango")
+    print(" \n Sua fruta é Morango ")
 elif resultadoUsuario == 3:
     print(" \n Sua fruta é Banana")
 
+elif resultadoUsuario == 4:
+    print("Sua Fruta é uma Laranja")
+
+elif resultadoUsuario == 5:
+    print( " \n É uma maça")
+
+elif resultadoUsuario == 6:
+    print(" \n É uma Melancia")
+
 else:
-    print( " \n É uma laranja")
+    print( " \n É uma Abacaxi")
 
 
 
